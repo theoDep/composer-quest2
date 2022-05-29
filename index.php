7a25c42d1fb49c484611868f8040dacabfe0cf75
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-05-29 11:28:09 +0200

    finish quest

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'vendor/autoload.php';
+use App\Wcs\Hello;
+
+echo Hello::say();
---
+++ src/wcs/Hello.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Wcs;
+
+class Hello
+{
+  public static function say()
+  {
+    return 'Hello world !';
+  }
+}
